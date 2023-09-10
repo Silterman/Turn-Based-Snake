f38e928290903477919a7b691906ff260cccde06
Axel <44074386+Silterman@users.noreply.github.com>
2023-09-10 18:57:32 +0200

    Update snakeGame.py
    - Removed double move when eating
    - Made collision checker relative to snake, and not grid.
    - Moved updateGrid function to gameGrid class (makes more sense that way)

--- snakeGame.py
+++ snakeGame.py
@@ -1,10 +1,14 @@
 import random
 from pprint import pprint
 
+#to do
+# make it clear where the head of the snake is
+
 class snake:
     def __init__(self, size):
         self.body = [(int((size-1)/2),int((size-1)/2))]
         self.curDir = None
+        self.score = 0
     def inputDetector(self, inp, game):
         turnTranslate = {"up": [-1, 0], "down": [1, 0], "left": [0, -1], "right":[0, 1]}
         if self.validTurn(turnTranslate[inp]):
@@ -14,23 +18,24 @@ class snake:
             print("Unable to U-Turn, choose another direction."); inp = str(input("What direction?")).lower()
             self.inputDetector(inp, game)
     def move(self, game): #moving snake coods
+        aCollision=False
         #checking for snake collision
         if game.grid[(self.body[0][0]+self.curDir[0])%game.size][(self.body[0][1]+self.curDir[1])%game.size] == 1:
             print("Collision detected - GAME OVER!")
+            print("Score:", self.score)
             quit()
         #checking for apple collision
         if game.grid[(self.body[0][0]+self.curDir[0])%game.size][(self.body[0][1]+self.curDir[1])%game.size] == 2:
-            #replacing apple with snake part
-            self.body.insert(0, ((self.body[0][0]+self.curDir[0])%game.size,(self.body[0][1]+self.curDir[1])%game.size)) 
+            aCollision = self.body[-1]
+            self.score += 1
             game.generateApple(self)
         game.grid[self.body[-1][0]%game.size][self.body[-1][1]%game.size] = 0 #setting tail on grid to 0 -> need to do it now while we still know coods
         for i in range(1, len(self.body)): #moving body one by one starting at the tail like a linked list
             self.body[-i] = self.body[-i-1]
         self.body[0] = (self.body[0][0]+self.curDir[0], self.body[0][1]+self.curDir[1]) #moving head based on direction
-        self.updateGrid(game)
-    def updateGrid(self, game) -> None: #redrawing grid/array
-        for xy in self.body:
-            game.grid[xy[0]%game.size][xy[1]%game.size] = 1
+        if aCollision:
+            self.body.append(aCollision)
+        game.updateGrid(self)
     def validTurn(self, inp) -> bool: #only checks if you do a uturn
         if self.curDir is None:
             return True
@@ -43,13 +48,17 @@ class gameGrid:
     def __init__(self, size, snake) -> None:
         self.size = size
         self.grid = [[0]*size for _ in range(size)]
-        self.grid[snake.body[0][0]][snake.body[0][1]] = 1
+        self.grid[snake.body[0][0]][snake.body[0][1]] = 9
         self.generateApple(snake)
     def generateApple(self, snake):
         x, y = random.randint(0, self.size-1), random.randint(0, self.size-1)
-        if self.grid[x][y] != 1:
+        if (x, y) not in snake.body:
             self.grid[x][y] = 2
         else: self.generateApple(snake)
+    def updateGrid(self, snake) -> None: #redrawing grid/array
+        self.grid[snake.body[0][0]%self.size][snake.body[0][1]%self.size] = 9
+        for xy in snake.body[1:]:
+            self.grid[xy[0]%self.size][xy[1]%self.size] = 1
 
 size = int(input("What size do you want to play with? Uneven integers only."))
 
